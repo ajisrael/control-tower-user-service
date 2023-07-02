155fa4aef756ebd80ea0715f2bf3e1bf05bf427f
ajisrael <43039187+ajisrael@users.noreply.github.com>
2023-07-02 05:09:41 -0400

    f   - added open api documentation to query api

--- src/main/java/control/tower/user/service/query/rest/UsersQueryController.java
+++ src/main/java/control/tower/user/service/query/rest/UsersQueryController.java
@@ -3,29 +3,37 @@ package control.tower.user.service.query.rest;
 import control.tower.user.service.query.queries.FindAllUsersQuery;
 import control.tower.user.service.query.queries.FindUserQuery;
 import control.tower.user.service.query.querymodels.UserQueryModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User Query API")
 public class UsersQueryController {
 
     @Autowired
     QueryGateway queryGateway;
 
     @GetMapping
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get users")
     public List<UserQueryModel> getUsers() {
         return queryGateway.query(new FindAllUsersQuery(),
                 ResponseTypes.multipleInstancesOf(UserQueryModel.class)).join();
     }
 
     @GetMapping(params = "userId")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get user by id")
     public UserQueryModel getUser(String userId) {
         return queryGateway.query(new FindUserQuery(userId),
                 ResponseTypes.instanceOf(UserQueryModel.class)).join();
