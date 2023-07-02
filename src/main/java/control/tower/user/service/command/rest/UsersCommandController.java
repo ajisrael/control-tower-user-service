e32a1d4430c87438d3103c141190937f0998c44d
ajisrael <43039187+ajisrael@users.noreply.github.com>
2023-07-02 05:02:38 -0400

    f   - added open api documentation to command api

--- src/main/java/control/tower/user/service/command/rest/UsersCommandController.java
+++ src/main/java/control/tower/user/service/command/rest/UsersCommandController.java
@@ -4,25 +4,28 @@ import control.tower.user.service.command.commands.CreateUserCommand;
 import control.tower.user.service.command.commands.RemoveUserCommand;
 import control.tower.user.service.command.rest.requests.CreateUserRequestModel;
 import control.tower.user.service.command.rest.requests.RemoveUserRequestModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User Command API")
 public class UsersCommandController {
 
     @Autowired
     private CommandGateway commandGateway;
 
     @PostMapping
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create user")
     public String createUser(@Valid @RequestBody CreateUserRequestModel createUserRequestModel) {
         CreateUserCommand createUserCommand = CreateUserCommand.builder()
                 .userId(UUID.randomUUID().toString())
@@ -37,12 +40,15 @@ public class UsersCommandController {
     }
 
     @DeleteMapping
-    public String removeUser(@Valid @RequestBody RemoveUserRequestModel removeUserRequestModel) {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Remove user")
+    public void removeUser(@Valid @RequestBody RemoveUserRequestModel removeUserRequestModel) {
         RemoveUserCommand removeUserCommand = RemoveUserCommand.builder()
                 .userId(removeUserRequestModel.getUserId())
                 .build();
 
-        return commandGateway.sendAndWait(removeUserCommand);
+        commandGateway.sendAndWait(removeUserCommand);
     }
 }
 
